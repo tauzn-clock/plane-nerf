87d96ca42a5d947767a612ce6179c476fcb91c19
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2024-01-09 12:44:42 +0000

    spoof input and target as dataparser

--- plane_nerf/plane_nerf_utils.py
+++ plane_nerf/plane_nerf_utils.py
@@ -12,23 +12,63 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nerfstudio.data.scene_box import SceneBox
+from typing import Any, Dict, List, Literal, Optional, Type
 from jaxtyping import Float
 from torch import Tensor
+import torch
 
-class PlaneSceneBox(SceneBox):
-    @staticmethod
-    def get_normalized_positions(positions: Float[Tensor, "*batch 3"], aabb: Float[Tensor, "2 3"]):
-        """Return normalized positions in range [0, 1] based on the aabb axis-aligned bounding box.
+def invert_SO3xR3_pose(transform: Float[Tensor, "3 4"]) -> Float[Tensor, "3 4"]:
+    """Inverts a 3x4 pose matrix.
 
-        Args:
-            positions: the xyz positions
-            aabb: the axis-aligned bounding box
-        """
-        aabb_lengths = aabb[1] - aabb[0]
-        normalized_positions = (positions - aabb[0]) / aabb_lengths
+    Args:
+        transform: a 3x4 pose matrix
 
-        #Set the z coordinate to 0.5
-        normalized_positions = normalized_positions.at[:, 2].set(0.5)
+    Returns:
+        The inverse of the pose matrix.
+    """
+    R = transform[:3, :3]
+    t = transform[:3, 3]
+    return torch.cat([R.t(), -R.t() @ t[:, None]], dim=1)
 
-        return normalized_positions
+def transform_original_space_to_pose(
+    poses: Float[Tensor, "num_poses 3 4"],
+    applied_transform: Float[Tensor, "3 4"],
+    applied_scale: float,
+    camera_convention: Literal["opengl", "opencv"] = "opencv",
+) -> Float[Tensor, "num_poses 3 4"]:
+    """
+    Transforms the original world coordinate system to the poses in the transformed space.
+    Args:
+        poses: Poses in the transformed space
+        applied_transform: Transform matrix applied in the data processing step
+        applied_scale: Scale used in the data processing step
+        camera_convention: Camera system convention used for the transformed poses
+    Returns:
+        Original poses
+    """
+    output_poses = torch.cat(
+        (
+            poses,
+            torch.tensor([[[0, 0, 0, 1]]], dtype=poses.dtype, device=poses.device).repeat_interleave(len(poses), 0),
+        ),
+        1,
+    )
+    transform = torch.cat(
+        (
+            applied_transform,
+            torch.tensor([[0, 0, 0, 1]], dtype=applied_transform.dtype, device=applied_transform.device),
+        ),
+        0,
+    )
+        
+    output_poses = torch.einsum("ij,bjk->bik", transform, output_poses)
+    
+    output_poses[..., :3, 3] *= applied_scale
+    
+    if camera_convention == "opencv":
+        raise ValueError(f"Camera convention {camera_convention} not yet supported.")
+    elif camera_convention == "opengl":
+        pass
+    else:
+        raise ValueError(f"Camera convention {camera_convention} is not supported.")
+    return output_poses[:, :3]
