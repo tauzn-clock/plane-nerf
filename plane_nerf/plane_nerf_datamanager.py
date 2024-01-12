64ae0c2dbc2e985e596f76e399a0aebeb0044372
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2024-01-12 11:16:34 +0000

    load camera params through training data

--- plane_nerf/plane_nerf_datamanager.py
+++ plane_nerf/plane_nerf_datamanager.py
@@ -3,8 +3,8 @@ Template DataManager
 """
 
 from dataclasses import dataclass, field
-from typing import Dict, Literal, Tuple, Type, Union
-
+from typing import Dict, Literal, Tuple, Type, Union, List
+from copy import deepcopy
 import torch
 
 from nerfstudio.cameras.rays import RayBundle
@@ -12,7 +12,13 @@ from nerfstudio.data.datamanagers.base_datamanager import (
     VanillaDataManager,
     VanillaDataManagerConfig,
 )
-
+from nerfstudio.data.utils.dataloaders import (
+    CacheDataloader,
+    FixedIndicesEvalDataloader,
+    RandIndicesEvalDataloader,
+)
+from nerfstudio.utils.rich_utils import CONSOLE
+from nerfstudio.model_components.ray_generators import RayGenerator
 
 @dataclass
 class PlaneNerfDataManagerConfig(VanillaDataManagerConfig):
@@ -71,3 +77,32 @@ class PlaneNerfDataManager(VanillaDataManager):
         print(ray_indices[0])
         ray_bundle = self.train_ray_generator(ray_indices)
         return ray_bundle, batch
+    
+    def setup_train(self):
+        """Sets up the data loaders for training"""
+        assert self.train_dataset is not None
+        CONSOLE.print("Setting up training dataset...")
+        self.train_image_dataloader = CacheDataloader(
+            self.train_dataset,
+            num_images_to_sample_from=self.config.train_num_images_to_sample_from,
+            num_times_to_repeat_images=self.config.train_num_times_to_repeat_images,
+            device=self.device,
+            num_workers=self.world_size * 4,
+            pin_memory=True,
+            collate_fn=self.config.collate_fn,
+            exclude_batch_keys_from_device=self.exclude_batch_keys_from_device,
+        )
+        self.iter_train_image_dataloader = iter(self.train_image_dataloader)
+        self.train_pixel_sampler = self._get_pixel_sampler(self.train_dataset, self.config.train_num_rays_per_batch)
+        self.train_ray_generator = RayGenerator(self.train_dataset.cameras.to(self.device))
+
+        self.fixed_indices_train_dataloader = FixedIndicesEvalDataloader(
+            input_dataset=self.train_dataset,
+            device=self.device,
+            num_workers=self.world_size * 4,
+        )
+        self.train_dataloader = RandIndicesEvalDataloader(
+            input_dataset=self.train_dataset,
+            device=self.device,
+            num_workers=self.world_size * 4,
+        )
