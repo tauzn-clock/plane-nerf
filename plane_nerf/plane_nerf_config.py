2b27598b790dcb4fe38b2a5ff963b4f41ccf4817
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2024-02-11 18:08:32 +0000

    simplified plane nerf method

--- plane_nerf/plane_nerf_config.py
+++ plane_nerf/plane_nerf_config.py
@@ -62,3 +62,47 @@ plane_nerf_method = MethodSpecification(
     ),
     description="Plane Nerf Method",
 )
+
+plane_nerf_simplified_method = MethodSpecification(
+    config=TrainerConfig(
+        method_name="plane-nerf-simplified",
+        steps_per_eval_batch=500,
+        steps_per_save=2000,
+        max_num_iterations=10000,
+        mixed_precision=True,
+        pipeline=PlaneNerfPipelineConfig(
+            datamanager=PlaneNerfDataManagerConfig(
+                dataparser=NerfstudioDataParserConfig(
+                    train_split_fraction = 1.0
+                ),
+                train_num_rays_per_batch=4096,
+                eval_num_rays_per_batch=4096,
+            ),
+            model=PlaneNerfConfig(
+                eval_num_rays_per_chunk=1 << 15,
+                hidden_dim = 32,
+                hidden_dim_color = 32,
+                hidden_dim_transient = 32,
+                max_res = 256
+            ),
+        ),
+        optimizers={
+            # TODO: consider changing optimizers depending on your custom method
+            "proposal_networks": {
+                "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+                "scheduler": ExponentialDecaySchedulerConfig(lr_final=0.0001, max_steps=200000),
+            },
+            "fields": {
+                "optimizer": RAdamOptimizerConfig(lr=1e-2, eps=1e-15),
+                "scheduler": ExponentialDecaySchedulerConfig(lr_final=1e-4, max_steps=50000),
+            },
+            "camera_opt": {
+                "optimizer": AdamOptimizerConfig(lr=1e-3, eps=1e-15),
+                "scheduler": ExponentialDecaySchedulerConfig(lr_final=1e-4, max_steps=5000),
+            },
+        },
+        viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
+        vis="viewer",
+    ),
+    description="Plane Nerf Method (Lower Detail)",
+)
