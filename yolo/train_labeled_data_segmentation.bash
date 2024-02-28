301cd3799c099a493174660a5a998fb890070075
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2024-02-28 16:30:47 +0000

    train yolo models

---
+++ yolo/train_labeled_data_segmentation.bash
@@ -0,0 +1,11 @@
+#Get the directory the bash file is currently in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo $DIR
+
+
+yolo task=segment \
+mode=train \
+model=yolov8m-seg.pt \
+data=$DIR/jackal_seg-2/data.yaml \
+epochs=100 \
+imgsz=640
