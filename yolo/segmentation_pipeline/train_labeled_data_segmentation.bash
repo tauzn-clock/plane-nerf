128e88296ff16654edb73debe1196da6db10a25c
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2024-03-11 13:52:58 +0000

    training update

--- yolo/segmentation_pipeline/train_labeled_data_segmentation.bash
+++ yolo/segmentation_pipeline/train_labeled_data_segmentation.bash
@@ -5,11 +5,12 @@ echo $DIR
 current_time=$(date +"%Y-%m-%d_%H-%M-%S")
 data_dir=$DIR'/../../data/jackal_floor_training_data_1/yolo'
 
+cd $data_dir
+
 yolo task=segment \
 mode=train \
 model=yolov8m-seg.pt \
 data=$data_dir'/data.yaml' \
 epochs=100 \
 imgsz=640 \
-project=data_dir \
-name=train_$current_time
+name=model_$current_time
