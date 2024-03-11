9c79ba1c98ffddd028d74b52afdfe6bd9a034670
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2024-03-11 13:42:47 +0000

    get yolo data

--- yolo/segmentation_pipeline/train_labeled_data_segmentation.bash
+++ yolo/segmentation_pipeline/train_labeled_data_segmentation.bash
@@ -2,10 +2,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo $DIR
 
+current_time=$(date +"%Y-%m-%d_%H-%M-%S")
+data_dir=$DIR'/../../data/jackal_floor_training_data_1/yolo'
 
 yolo task=segment \
 mode=train \
 model=yolov8m-seg.pt \
-data=$DIR/../../data/jackal_floor_training_data_1/yolo/data.yaml \
+data=$data_dir'/data.yaml' \
 epochs=100 \
-imgsz=640
+imgsz=640 \
+project=data_dir \
+name=train_$current_time
